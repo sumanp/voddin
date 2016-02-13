faf4775f006a543d16697bf3e8ec676cefa15672
sumanp <sumanpuri55@gmail.com>
2016-02-13 17:08:08 +0530

    Added task model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
 	has_many :notes, dependent: :destroy
 	belongs_to :user
+	has_many :tasks, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211092534) do
+ActiveRecord::Schema.define(version: 20160212124109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 20160211092534) do
     t.datetime "end_date"
     t.datetime "start_date"
   end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "done"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -59,4 +69,5 @@ ActiveRecord::Schema.define(version: 20160211092534) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "notes", "projects"
+  add_foreign_key "tasks", "projects"
 end
